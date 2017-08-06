00741c33dea478ed2d17aea89f63a6ee1c454d99
Karan Joshi <karanjoshi31@live.com>
2017-08-06 12:49:33 -0500

    Cleared list.js file and added jQuery integration to html list file

--- JS/list.js
+++ JS/list.js
@@ -1,24 +1,5 @@
-
-function getText(){
-
-  var inputText = document.getElementById('input-field').value;
-
-  if(inputText !== ""){
-    document.getElementById('input-field').value = "";
-
-    var node = document.createElement('li');
-    var text = document.createTextNode(inputText);
-    node.appendChild(text);
-
-    var list = document.getElementById('todo-list');
-    list.appendChild(node);
-  }
-
-}
-
-function handle(e){
-  if(e.keycode == 13 || e.which == 13){
-    
-    getText();
-  }
+if($){
+  console.log("jQuery loaded");
+}else{
+  console.log("jQuery not loaded");
 }
