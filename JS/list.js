9310dc705a1afd05b6f8421eb3d08ed17b80fc6f
Karan Joshi <karanjoshi31@live.com>
2017-08-06 12:05:28 -0500

    completed logic of adding to list

--- JS/list.js
+++ JS/list.js
@@ -1,9 +1,17 @@
 
-
-
 function getText(){
-  var inputText = document.getElementById('input-field').value;
-  console.log(inputText)
 
-  document.getElementById('input-field').value = "";
+  var inputText = document.getElementById('input-field').value;
+
+  if(inputText !== ""){
+    document.getElementById('input-field').value = "";
+
+    var node = document.createElement('li');
+    var text = document.createTextNode(inputText);
+    node.appendChild(text);
+
+    var list = document.getElementById('todo-list');
+    list.appendChild(node);
+  }
+
 }
