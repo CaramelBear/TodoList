83d88bd2fb3fe21d83d7937bf1e0c1ccb886ebd2
Karan Joshi <karanjoshi31@live.com>
2017-08-07 19:10:09 -0500

    integrated vue.js

--- JS/add.js
+++ JS/add.js
@@ -1,33 +1,8 @@
-if($){
-  console.log("jQuery loaded");
-}else{
-  console.log("jQuery not loaded");
-}
+var todoText = "Please Enter a Todo:";
 
-
-var inputField = $('#input-field');
-var addButton = $('#add-button');
-var listContainer = $('#todo-list');
-
-function createListItem(text){
-  if(text!== ""){
-    return $("<li></li>").text(text);
+var app = new Vue({
+  el: "#master-container",
+  data:{
+    message: todoText
   }
-
-
-}
-
-function getText(){
-  return inputField.val();
-}
-
-function clearInput(){
-  inputField.val("");
-}
-
-$(addButton).click(function(){
-  var newText = getText();
-  var newListItem = createListItem(newText);
-  $(listContainer).append(newListItem);
-  clearInput();
 });
