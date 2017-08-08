0ccbbc8e4a2d496644eaa9cfc1d292791c4c5933
Karan Joshi <karanjoshi31@live.com>
2017-08-07 19:49:15 -0500

    limited functionality for add button

--- JS/add.js
+++ JS/add.js
@@ -1,8 +1,28 @@
 var todoText = "Please Enter a Todo:";
+var todoList = [];
+
+
 
 var app = new Vue({
   el: "#master-container",
   data:{
     message: todoText
+  },
+  methods:{
+    add: function(){
+      getInput();
+      clearInput();
+    }
   }
 });
+
+function clearInput(){
+  document.getElementById("input-field").value = "";
+}
+
+function getInput(){
+  var inputField = document.getElementById('input-field').value;
+  if(inputField !== ""){
+    console.log(inputField);
+  }
+}
